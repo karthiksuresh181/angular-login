5150daa674e73143c6e029e5f6f52bd496b713f4
karthiksuresh181 <karthu.karthu1@gmail.com>
2023-03-05 14:44:52 +0530

    added angular material and layout comps

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LayoutComponent, HeaderComponent],
+  exports: [LayoutComponent, CommonModule]
 })
 export class SharedModule { }
